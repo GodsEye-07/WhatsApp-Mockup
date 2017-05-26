f50ccf69ddcf16e1e90c875e201084a5cf2f944a
Ayush Verma <ayush.verma2016@vitstudent.ac.in>
2017-05-27 00:02:39 +0530

    settings tab changed

--- WhatsApp copy/SettingsVC.swift
+++ WhatsApp copy/SettingsVC.swift
@@ -8,12 +8,20 @@
 
 import UIKit
 
-class SettingsVC: UIViewController {
+class SettingsVC: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
+    
+    var settingData = [["Ayush Verma"],["Starred Messages","Whatsapp Web/Desktop"],["Account","Chats","Notifications","Data and Storage Usage"],["About and Help","Tell a Friend"]]
+    
+    var icons = ["S1.png","S2.png","S3.png","S4.png","S5.png","S6.png","S7.png","S8.png"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +29,51 @@ class SettingsVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 4
     }
-    */
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return settingData[section].count
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.section == 0{
+            return 73
+        }
+        else{
+            return 44
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 44
+    }
+    
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cellIdentifier = "cell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! settingCustomCell
+        
+        cell.settingLabel.text = settingData[indexPath.section][indexPath.row]
+        
+        cell.settingImage.backgroundColor = UIColor(red: CGFloat(arc4random()%255)/255.0, green: CGFloat(arc4random()%255)/255.0, blue: CGFloat(arc4random()%255)/255.0, alpha: 1.0)
+        
+        if indexPath.section == 0{
+            cell.settingImage.layer.cornerRadius = 29.25
+        }
+        else{
+            cell.settingImage.layer.cornerRadius = 7
+        }
+        
+        cell.settingImage.clipsToBounds = true
+        
+        return cell
+    }
+    
 
+    
 }
---
+++ WhatsApp copy/settingCustomCell.swift
@@ -0,0 +1,30 @@
+//
+//  settingCustomCell.swift
+//  WhatsApp copy
+//
+//  Created by Ayush Verma on 26/05/17.
+//  Copyright © 2017 ayush. All rights reserved.
+//
+
+import UIKit
+
+class settingCustomCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var settingImage: UIImageView!
+    @IBOutlet weak var settingLabel: UILabel!
+    
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
